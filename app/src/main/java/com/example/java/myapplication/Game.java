47cbc4bdee9d63860b8d08e2b5a82a0a3a270bbc
Machaca <machacahamilton@msn.com>
2018-03-13 09:32:12 +0100

    Navbar hidden

--- app/src/main/java/com/example/java/myapplication/Game.java
+++ app/src/main/java/com/example/java/myapplication/Game.java
@@ -1,21 +1,8 @@
 package com.example.java.myapplication;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.security.AccessController;
 
 public class Game extends Activity {
 
@@ -23,7 +10,6 @@ public class Game extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         //Recoger el intent
         Bundle bundle = getIntent().getExtras();
         String user = bundle.getString("USUARIO");
