3b784a47bd035dd0f87811456d92806e271dd140
colora <java@ozanam.com>
2018-03-23 12:58:44 +0100

    Customlist

--- app/src/main/java/com/example/java/myapplication/CustomList.java
+++ app/src/main/java/com/example/java/myapplication/CustomList.java
@@ -1,10 +1,15 @@
 package com.example.java.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,11 +36,13 @@ public class CustomList extends BaseAdapter {
         return position;
     }
 
+    @SuppressLint("ResourceAsColor")
     public View getView(int position, View arg1, ViewGroup viewGroup) {
 
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row = inflater.inflate(R.layout.activity_custom_list, viewGroup, false);
 
+        RelativeLayout rl = (RelativeLayout) row.findViewById(R.id.rl);
 
         TextView idsala = (TextView) row.findViewById(R.id.idsala);
         TextView nombresala = (TextView) row.findViewById(R.id.sala);
@@ -50,9 +57,16 @@ public class CustomList extends BaseAdapter {
             if(position!=0) {
                 numsala.setText(String.valueOf(position));
                 jugadores.setText(listItem.get(position).get(3).toString() + "/" + listItem.get(position).get(2).toString());
+                rl.setBackgroundColor(ContextCompat.getColor(mContext, R.color.listado));
 
             }else{
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    rl.setBackground(ContextCompat.getDrawable(mContext,R.drawable.newgamebutton));
+                }else{
+                    rl.setBackgroundDrawable(ContextCompat.getDrawable(mContext,R.drawable.newgamebutton));
+
+                }
                 numsala.setText("");
                 jugadores.setText("");
 
